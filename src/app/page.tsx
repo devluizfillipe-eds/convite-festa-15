eff8b0638fe550c573233c90ecc7db7ad3c245bf
devluizfillipe-eds <devluizfillipe@gmail.com>
2026-03-11 10:55:32 -0300

    feat: finalização convite personalizado para Laura

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
   const videoRef = useRef<HTMLVideoElement>(null);
   const [started, setStarted] = useState(false);
   const [showSecondScreen, setShowSecondScreen] = useState(false);
+  const [showPresentScreen, setShowPresentScreen] = useState(false);
 
   const handlePlay = () => {
     if (videoRef.current && !started) {
@@ -30,31 +31,21 @@ export default function Home() {
               pointerEvents: showSecondScreen ? "none" : "auto",
               transition: "opacity 0.8s",
             }}
+            onClick={handlePlay}
           >
-            <div className={styles.videoContainer}>
-              <video
-                ref={videoRef}
-                className={styles.video}
-                src="/video01animação.mp4"
-                playsInline
-                preload="auto"
-                controls={false}
-                poster=""
-                onClick={handlePlay}
-                style={{ pointerEvents: started ? "auto" : "none" }}
-                onEnded={handleVideoEnd}
-              />
-              {/* Botão invisível apenas sobre a área do texto */}
-              {!started && (
-                <button
-                  className={styles.invisibleBtn}
-                  onClick={handlePlay}
-                  aria-label="Play video"
-                />
-              )}
-            </div>
+            <video
+              ref={videoRef}
+              className={styles.fullscreenVideo}
+              src="/video01animação.mp4"
+              playsInline
+              preload="auto"
+              controls={false}
+              poster=""
+              style={{ pointerEvents: started ? "auto" : "none" }}
+              onEnded={handleVideoEnd}
+            />
           </div>
-          {/* Segunda tela: imagem de fundo */}
+          {/* Segunda tela: imagem de fundo e botões */}
           <div
             className={styles.secondScreen}
             style={{
@@ -63,7 +54,50 @@ export default function Home() {
               transition: "opacity 0.8s",
             }}
           >
-            <div className={styles.bgImage} />
+            <div className={styles.bgImage}>
+              {/* Botões visíveis para ajuste */}
+              <button
+                className={styles.circleBtn1}
+                onClick={() => {
+                  window.open(
+                    "https://wa.me/5531988510531?text=ol%C3%A1%20%20minha%20presen%C3%A7a%20est%C3%A1%20confirmada",
+                    "_blank",
+                  );
+                }}
+              >
+                1
+              </button>
+              <button
+                className={styles.circleBtn2}
+                onClick={() => {
+                  window.open(
+                    "https://www.google.com/maps?q=Espa%C3%A7o+Bells+recep%C3%A7%C3%B5es+e+eventos+-+R.+Virg%C3%ADlio+de+Melo+Franco,+304+-+Vila+Sao+Paulo,+Contagem+-+MG,+32210-350&ftid=0xa6973239194621:0x2f17a26aa31289d2&entry=gps&shh=CAE&lucs=,94297699,94284505,94231188,94280568,47071704,94218641,94282134,94286869&g_ep=CAISEjI2LjEwLjIuODc3MzE3OTEwMBgAINeCAypILDk0Mjk3Njk5LDk0Mjg0NTA1LDk0MjMxMTg4LDk0MjgwNTY4LDQ3MDcxNzA0LDk0MjE4NjQxLDk0MjgyMTM0LDk0Mjg2ODY5QgJCUg%3D%3D&skid=9ee14688-cffb-403a-ba23-11ef5732eac3&g_st=ic",
+                    "_blank",
+                  );
+                }}
+              >
+                2
+              </button>
+              <button
+                className={styles.circleBtn3}
+                onClick={() => {
+                  setShowPresentScreen(true);
+                }}
+              >
+                3
+              </button>
+            </div>
+          </div>
+          {/* Terceira tela: imagem de presentes */}
+          <div
+            className={styles.presentScreen}
+            style={{
+              opacity: showPresentScreen ? 1 : 0,
+              pointerEvents: showPresentScreen ? "auto" : "none",
+              transition: "opacity 0.8s",
+            }}
+          >
+            <div className={styles.presentBgImage}></div>
           </div>
         </div>
       </main>
