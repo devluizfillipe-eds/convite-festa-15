43293a92e9d8b34b5d7f650beb61a7d7ff785079
devluizfillipe-eds <devluizfillipe@gmail.com>
2026-03-11 11:33:32 -0300

    feat: ajuste dos botões e sobreposição

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,37 +55,41 @@ export default function Home() {
             }}
           >
             <div className={styles.bgImage}>
-              {/* Botões visíveis para ajuste */}
-              <button
-                className={styles.circleBtn1}
-                onClick={() => {
-                  window.open(
-                    "https://wa.me/5531988510531?text=ol%C3%A1%20%20minha%20presen%C3%A7a%20est%C3%A1%20confirmada",
-                    "_blank",
-                  );
-                }}
-              >
-                1
-              </button>
-              <button
-                className={styles.circleBtn2}
-                onClick={() => {
-                  window.open(
-                    "https://www.google.com/maps?q=Espa%C3%A7o+Bells+recep%C3%A7%C3%B5es+e+eventos+-+R.+Virg%C3%ADlio+de+Melo+Franco,+304+-+Vila+Sao+Paulo,+Contagem+-+MG,+32210-350&ftid=0xa6973239194621:0x2f17a26aa31289d2&entry=gps&shh=CAE&lucs=,94297699,94284505,94231188,94280568,47071704,94218641,94282134,94286869&g_ep=CAISEjI2LjEwLjIuODc3MzE3OTEwMBgAINeCAypILDk0Mjk3Njk5LDk0Mjg0NTA1LDk0MjMxMTg4LDk0MjgwNTY4LDQ3MDcxNzA0LDk0MjE4NjQxLDk0MjgyMTM0LDk0Mjg2ODY5QgJCUg%3D%3D&skid=9ee14688-cffb-403a-ba23-11ef5732eac3&g_st=ic",
-                    "_blank",
-                  );
-                }}
-              >
-                2
-              </button>
-              <button
-                className={styles.circleBtn3}
-                onClick={() => {
-                  setShowPresentScreen(true);
-                }}
-              >
-                3
-              </button>
+              {/* Botões só aparecem na tela 2 */}
+              {showSecondScreen && (
+                <>
+                  <button
+                    className={styles.circleBtn1}
+                    onClick={() => {
+                      window.open(
+                        "https://wa.me/5531988510531?text=ol%C3%A1%20%20minha%20presen%C3%A7a%20est%C3%A1%20confirmada",
+                        "_blank",
+                      );
+                    }}
+                  >
+                    1
+                  </button>
+                  <button
+                    className={styles.circleBtn2}
+                    onClick={() => {
+                      window.open(
+                        "https://www.google.com/maps?q=Espa%C3%A7o+Bells+recep%C3%A7%C3%B5es+e+eventos+-+R.+Virg%C3%ADlio+de+Melo+Franco,+304+-+Vila+Sao+Paulo,+Contagem+-+MG,+32210-350&ftid=0xa6973239194621:0x2f17a26aa31289d2&entry=gps&shh=CAE&lucs=,94297699,94284505,94231188,94280568,47071704,94218641,94282134,94286869&g_ep=CAISEjI2LjEwLjIuODc3MzE3OTEwMBgAINeCAypILDk0Mjk3Njk5LDk0Mjg0NTA1LDk0MjMxMTg4LDk0MjgwNTY4LDQ3MDcxNzA0LDk0MjE4NjQxLDk0MjgyMTM0LDk0Mjg2ODY5QgJCUg%3D%3D&skid=9ee14688-cffb-403a-ba23-11ef5732eac3&g_st=ic",
+                        "_blank",
+                      );
+                    }}
+                  >
+                    2
+                  </button>
+                  <button
+                    className={styles.circleBtn3}
+                    onClick={() => {
+                      setShowPresentScreen(true);
+                    }}
+                  >
+                    3
+                  </button>
+                </>
+              )}
             </div>
           </div>
           {/* Terceira tela: imagem de presentes */}
